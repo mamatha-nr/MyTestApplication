1db995256a0879d0d9d4e80cfd25d97ac458622a
MAMATNAR <MAMATNAR@MAMATNAR-PC.in.oracle.com>
2019-04-15 17:01:18 +0530

    Commiting mamat

---
+++ CICDJava/src/com/ofss/Address.java
@@ -0,0 +1,26 @@
+package com.ofss;
+
+public class Address {
+	
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Address(int flatNo, String streetName, String city, long pincode) {
+		super();
+		this.flatNo = flatNo;
+		this.streetName = streetName;
+		this.city = city;
+		this.pincode = pincode;
+	}
+	private long pincode;
+	private int flatNo;
+	private String streetName;
+	@Override
+	public String toString() {
+		return "Address [pincode=" + pincode + ", flatNo=" + flatNo + ", streetName=" + streetName + ", city=" + city
+				+ "]";
+	}
+	private String city;
+}
